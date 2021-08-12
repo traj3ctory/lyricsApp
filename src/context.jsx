96500f557985c2666aa52ca4ca2d08f5f97b121c
traj3ctory <davidbenard.bd@gmail.com>
2021-08-12 03:11:39 +0100

    fix(cors-error): remove cors-anywhere link

--- src/context.jsx
+++ src/context.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 const Context = React.createContext();
 const reducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'SEARCH_TRACKS':
-        return {
-            ...state,
-            track_list: action.payload,
-            heading : 'SEarch Results'
-        };
+            return {
+                ...state,
+                track_list: action.payload,
+                heading: 'SEarch Results'
+            };
         default:
             return state;
     }
@@ -23,7 +23,12 @@ export class Provider extends Component {
     };
 
     componentDidMount() {
-        axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`, {
+            headers: {
+                Authorization: "Access-Control-Allow-Headers",
+                'Content-Type': 'application/json'
+            }
+        })
             .then(res => {
                 this.setState({ track_list: res.data.message.body.track_list });
             })
